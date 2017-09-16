cd9104548e94f496f4f7ddd9f4214f0a9a40351f
hya <hya_cz@163.com>
2017-09-16 15:44:42 +0800

    加入了数据库及各项配置

---
+++ app/src/main/java/com/coolweather/android/database/Province.java
@@ -0,0 +1,37 @@
+package com.coolweather.android.database;
+
+import org.litepal.crud.DataSupport;
+
+/**
+ * Created by 洪裕安 on 2017/9/16.
+ */
+
+public class Province extends DataSupport{
+    private int id;
+    private String proviceName;
+    private int proviceCode;
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getProviceName() {
+        return proviceName;
+    }
+
+    public void setProviceName(String proviceName) {
+        this.proviceName = proviceName;
+    }
+
+    public int getProviceCode() {
+        return proviceCode;
+    }
+
+    public void setProviceCode(int proviceCode) {
+        this.proviceCode = proviceCode;
+    }
+}
---
+++ app/src/main/java/com/coolweather/android/util/MyApplication.java
@@ -0,0 +1,24 @@
+package com.coolweather.android.util;
+
+import android.app.Application;
+import android.content.Context;
+
+import org.litepal.LitePal;
+
+/**
+ * Created by 洪裕安 on 2017/9/16.
+ */
+
+public class MyApplication extends Application {
+    private static Context context;
+
+    @Override
+    public void onCreate() {
+        context = getApplicationContext();
+        LitePal.initialize(context);
+    }
+
+    public static Context getContext(){
+        return context;
+    }
+}
